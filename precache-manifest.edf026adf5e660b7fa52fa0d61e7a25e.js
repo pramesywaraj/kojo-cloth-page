81fe3535e90d53ab240e8df63f91d25a2bc1057f
pramesywaraj <pramesywaraj@pahamify.com>
2022-06-04 23:47:34 +0700

    Updates

---
+++ precache-manifest.edf026adf5e660b7fa52fa0d61e7a25e.js
@@ -0,0 +1,346 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9988f053fdcb1c6929af62af1440843",
+    "url": "/kojo-cloth-page/index.html"
+  },
+  {
+    "revision": "c161e048589bfdb42cac",
+    "url": "/kojo-cloth-page/static/css/2.a8f673c4.chunk.css"
+  },
+  {
+    "revision": "67ab4866f5aa4c93b2db",
+    "url": "/kojo-cloth-page/static/css/main.9e8fa7f1.chunk.css"
+  },
+  {
+    "revision": "c161e048589bfdb42cac",
+    "url": "/kojo-cloth-page/static/js/2.1ae9eb50.chunk.js"
+  },
+  {
+    "revision": "6993fa7b13fb81f4bc70d0dc713d6897",
+    "url": "/kojo-cloth-page/static/js/2.1ae9eb50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67ab4866f5aa4c93b2db",
+    "url": "/kojo-cloth-page/static/js/main.f01b4f07.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/kojo-cloth-page/static/js/main.f01b4f07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ac43ca06b90ce9eb12d",
+    "url": "/kojo-cloth-page/static/js/runtime-main.ff3e084d.js"
+  },
+  {
+    "revision": "3beb8769416fceca3157c41172dc742b",
+    "url": "/kojo-cloth-page/static/media/1.3beb8769.jpeg"
+  },
+  {
+    "revision": "eb34680694b95e3d1287975907e181fa",
+    "url": "/kojo-cloth-page/static/media/10.eb346806.jpeg"
+  },
+  {
+    "revision": "efef1be8bd7caba600de864ef9f1bb6d",
+    "url": "/kojo-cloth-page/static/media/11.efef1be8.jpeg"
+  },
+  {
+    "revision": "e772abd086949c6fddd890e14a9781e5",
+    "url": "/kojo-cloth-page/static/media/12.e772abd0.jpeg"
+  },
+  {
+    "revision": "0db9dace439b5b853549735a10578ea5",
+    "url": "/kojo-cloth-page/static/media/13.0db9dace.jpeg"
+  },
+  {
+    "revision": "03a8ba8e4bd587ebf9b2b4148d6ce082",
+    "url": "/kojo-cloth-page/static/media/14.03a8ba8e.jpeg"
+  },
+  {
+    "revision": "ed4dcf10e975af92774bfe6c54dc1661",
+    "url": "/kojo-cloth-page/static/media/15.ed4dcf10.jpeg"
+  },
+  {
+    "revision": "f8d698062f092ada4f445cbecda7cc4b",
+    "url": "/kojo-cloth-page/static/media/16.f8d69806.jpeg"
+  },
+  {
+    "revision": "380056b6aa060d8e5b7a39ee46ed2012",
+    "url": "/kojo-cloth-page/static/media/17.380056b6.jpeg"
+  },
+  {
+    "revision": "e1dc7e83586eedf25426fca4e933d194",
+    "url": "/kojo-cloth-page/static/media/18.e1dc7e83.jpeg"
+  },
+  {
+    "revision": "e29a348366725247269f8203c868f5c2",
+    "url": "/kojo-cloth-page/static/media/19.e29a3483.jpeg"
+  },
+  {
+    "revision": "18144ab0d25d38a2f9f4fb267f169a1a",
+    "url": "/kojo-cloth-page/static/media/2.18144ab0.jpeg"
+  },
+  {
+    "revision": "e29a348366725247269f8203c868f5c2",
+    "url": "/kojo-cloth-page/static/media/20.e29a3483.jpeg"
+  },
+  {
+    "revision": "fa97775767ad787cba2d72be07513d12",
+    "url": "/kojo-cloth-page/static/media/21.fa977757.jpeg"
+  },
+  {
+    "revision": "a0d4da292c4629077bf9d01a69a7b7d7",
+    "url": "/kojo-cloth-page/static/media/22.a0d4da29.jpeg"
+  },
+  {
+    "revision": "723c1d484ea9d45e8d17d4b14a06bb1f",
+    "url": "/kojo-cloth-page/static/media/23.723c1d48.jpeg"
+  },
+  {
+    "revision": "c34c110cd42ee18ed5f11502f47ff392",
+    "url": "/kojo-cloth-page/static/media/24.c34c110c.jpeg"
+  },
+  {
+    "revision": "7b3397ea0fcaf8a41683ed806b652628",
+    "url": "/kojo-cloth-page/static/media/25.7b3397ea.jpeg"
+  },
+  {
+    "revision": "60e7412b2a1ffcd4554ca9928cca263f",
+    "url": "/kojo-cloth-page/static/media/26.60e7412b.jpeg"
+  },
+  {
+    "revision": "815fb1e42108edb8191aab463c2dd7ab",
+    "url": "/kojo-cloth-page/static/media/27.815fb1e4.jpeg"
+  },
+  {
+    "revision": "42741d0c1c74f40765e7ea91087c1c13",
+    "url": "/kojo-cloth-page/static/media/28.42741d0c.jpeg"
+  },
+  {
+    "revision": "ea7bf77000dbed89a372503baf438da0",
+    "url": "/kojo-cloth-page/static/media/29.ea7bf770.jpeg"
+  },
+  {
+    "revision": "2414f803dc509fb05a10da00ae25bf02",
+    "url": "/kojo-cloth-page/static/media/3.2414f803.jpeg"
+  },
+  {
+    "revision": "9e942db1bcec8b750dfb38abc82b950e",
+    "url": "/kojo-cloth-page/static/media/30.9e942db1.jpeg"
+  },
+  {
+    "revision": "ab549a31fada95d8620c66388d855c3f",
+    "url": "/kojo-cloth-page/static/media/31.ab549a31.jpeg"
+  },
+  {
+    "revision": "2c28c94b5da65f5d071acd3490c4547c",
+    "url": "/kojo-cloth-page/static/media/4.2c28c94b.jpeg"
+  },
+  {
+    "revision": "b6aa30f3526d2cad0d8481cb2407119f",
+    "url": "/kojo-cloth-page/static/media/5.b6aa30f3.jpeg"
+  },
+  {
+    "revision": "992675bfb1e65a0c4f5cffdf7f177ad9",
+    "url": "/kojo-cloth-page/static/media/6.992675bf.jpeg"
+  },
+  {
+    "revision": "b32080a805594da9511bffb5de0d4de1",
+    "url": "/kojo-cloth-page/static/media/7.b32080a8.jpeg"
+  },
+  {
+    "revision": "6c424e791b72cbedfa15007b5b38df97",
+    "url": "/kojo-cloth-page/static/media/8.6c424e79.jpeg"
+  },
+  {
+    "revision": "28b3303d5a145ae682b6df1ae85cc052",
+    "url": "/kojo-cloth-page/static/media/9.28b3303d.jpeg"
+  },
+  {
+    "revision": "430f1d982f3792164fb989914ecd6bfc",
+    "url": "/kojo-cloth-page/static/media/bag.430f1d98.svg"
+  },
+  {
+    "revision": "4f3fc6561abc1bd3073e8dfd13b67d0b",
+    "url": "/kojo-cloth-page/static/media/bg-landing.4f3fc656.jpg"
+  },
+  {
+    "revision": "d35c345e01775d3a60109f175a47ca0b",
+    "url": "/kojo-cloth-page/static/media/bracelet.d35c345e.svg"
+  },
+  {
+    "revision": "9b7806d3372f10b68b172b5acd16c8d8",
+    "url": "/kojo-cloth-page/static/media/example-1.9b7806d3.svg"
+  },
+  {
+    "revision": "affbc50f2b36c1062add67b320954923",
+    "url": "/kojo-cloth-page/static/media/example-10.affbc50f.svg"
+  },
+  {
+    "revision": "550741fb271dfae52f2c0b58ffbc264d",
+    "url": "/kojo-cloth-page/static/media/example-11.550741fb.svg"
+  },
+  {
+    "revision": "97a0a31f3df77385a79ea8bcf1bd3ba7",
+    "url": "/kojo-cloth-page/static/media/example-2.97a0a31f.svg"
+  },
+  {
+    "revision": "8ea4ce21dfed06c4b9b2e7b1ab0170f9",
+    "url": "/kojo-cloth-page/static/media/example-3.8ea4ce21.svg"
+  },
+  {
+    "revision": "ee9ff9b4741776611675eeade9f0db05",
+    "url": "/kojo-cloth-page/static/media/example-4.ee9ff9b4.svg"
+  },
+  {
+    "revision": "c5d587c8862ec1d07782f001a3851e22",
+    "url": "/kojo-cloth-page/static/media/example-5.c5d587c8.svg"
+  },
+  {
+    "revision": "2c4b2fdb9a2b70d6513ac15b7778924f",
+    "url": "/kojo-cloth-page/static/media/example-6.2c4b2fdb.svg"
+  },
+  {
+    "revision": "d0a5dd89a4363f4149aaf968795ed937",
+    "url": "/kojo-cloth-page/static/media/example-7.d0a5dd89.svg"
+  },
+  {
+    "revision": "c3705423df236f2d9527de15a810b9ce",
+    "url": "/kojo-cloth-page/static/media/example-8.c3705423.svg"
+  },
+  {
+    "revision": "2fc3e7df6f010de4c5acb116553c2c78",
+    "url": "/kojo-cloth-page/static/media/example-9.2fc3e7df.svg"
+  },
+  {
+    "revision": "9b130291b28d6f74195776a4e274f68e",
+    "url": "/kojo-cloth-page/static/media/hoodie.9b130291.svg"
+  },
+  {
+    "revision": "b41d92b84082753f55fb1da5ce85c991",
+    "url": "/kojo-cloth-page/static/media/ic-check.b41d92b8.svg"
+  },
+  {
+    "revision": "c566c110926a871ad062fd749fd98427",
+    "url": "/kojo-cloth-page/static/media/ic-email.c566c110.svg"
+  },
+  {
+    "revision": "038cf06770ea4efe20da178f69616cdc",
+    "url": "/kojo-cloth-page/static/media/ic-fb.038cf067.svg"
+  },
+  {
+    "revision": "f9a134de93a4b896659c12089ff96611",
+    "url": "/kojo-cloth-page/static/media/ic-google.f9a134de.svg"
+  },
+  {
+    "revision": "6a0a6e00109d12a876d3081f988341ff",
+    "url": "/kojo-cloth-page/static/media/ic-hours.6a0a6e00.svg"
+  },
+  {
+    "revision": "bfc320e9d679376780638163978681ab",
+    "url": "/kojo-cloth-page/static/media/ic-instagram.bfc320e9.svg"
+  },
+  {
+    "revision": "51c73195de21700667ba2c1fb7de40a8",
+    "url": "/kojo-cloth-page/static/media/ic-phone.51c73195.svg"
+  },
+  {
+    "revision": "30a35148802d65798a1ea77216b6d3ee",
+    "url": "/kojo-cloth-page/static/media/ic-pin.30a35148.svg"
+  },
+  {
+    "revision": "ba33d7348ba37b45d5f6ae0d8edad7d3",
+    "url": "/kojo-cloth-page/static/media/ic-twitter.ba33d734.svg"
+  },
+  {
+    "revision": "14bb5ca4f67ea4e9158d5272402dc2fc",
+    "url": "/kojo-cloth-page/static/media/ic-user.14bb5ca4.svg"
+  },
+  {
+    "revision": "61dc3fadd02971b5405648ea550bd517",
+    "url": "/kojo-cloth-page/static/media/ic-youtube.61dc3fad.svg"
+  },
+  {
+    "revision": "d6bd50f15a551bce46c009c1ca4ffdec",
+    "url": "/kojo-cloth-page/static/media/jaket-gunung.d6bd50f1.svg"
+  },
+  {
+    "revision": "4d6e7bd8327402640102643236281a99",
+    "url": "/kojo-cloth-page/static/media/jaket-motor.4d6e7bd8.svg"
+  },
+  {
+    "revision": "e7bf6785a38c982d4377e171251cd0d3",
+    "url": "/kojo-cloth-page/static/media/jaket-musim-dingin.e7bf6785.svg"
+  },
+  {
+    "revision": "062d65356fa991017b66eacf9827551f",
+    "url": "/kojo-cloth-page/static/media/jaket-parka.062d6535.svg"
+  },
+  {
+    "revision": "3f8f2f8f83af05e8e20b9cbbf574fc83",
+    "url": "/kojo-cloth-page/static/media/jaket-versity.3f8f2f8f.svg"
+  },
+  {
+    "revision": "af4f51033632f01ff111f5a66b7fb8ec",
+    "url": "/kojo-cloth-page/static/media/jaket-zipper.af4f5103.svg"
+  },
+  {
+    "revision": "39b829fcd3e9290efa75823c9424d47e",
+    "url": "/kojo-cloth-page/static/media/jas-almamater.39b829fc.svg"
+  },
+  {
+    "revision": "4b9b7671fca9c03cd0b257900d47680c",
+    "url": "/kojo-cloth-page/static/media/jas-kerja.4b9b7671.svg"
+  },
+  {
+    "revision": "211d35b0d6a755c13ba32d13883d4f80",
+    "url": "/kojo-cloth-page/static/media/kaos-fullprint.211d35b0.svg"
+  },
+  {
+    "revision": "b53f39401740dcb9ef314a5ddff19ee4",
+    "url": "/kojo-cloth-page/static/media/kaos-kombinasi.b53f3940.svg"
+  },
+  {
+    "revision": "d6632a1290f675cd1f7a54db5a379fa5",
+    "url": "/kojo-cloth-page/static/media/kaos-oblong.d6632a12.svg"
+  },
+  {
+    "revision": "67f5273fafc9ba08efe8148c88daf7ae",
+    "url": "/kojo-cloth-page/static/media/kaos-panjang-raglan.67f5273f.svg"
+  },
+  {
+    "revision": "7bc9fbafc634b533e09fef84d0b40558",
+    "url": "/kojo-cloth-page/static/media/kaos-polo.7bc9fbaf.svg"
+  },
+  {
+    "revision": "93f1a69137993a43764ef4ba28b79bc2",
+    "url": "/kojo-cloth-page/static/media/kemeja-kerja-jurusan.93f1a691.svg"
+  },
+  {
+    "revision": "4f5177e739ca1b858b7ac22f67aee3fb",
+    "url": "/kojo-cloth-page/static/media/kojo-human-1.4f5177e7.svg"
+  },
+  {
+    "revision": "e12144e17035c05715959b5dc26489f4",
+    "url": "/kojo-cloth-page/static/media/kojo-human-2.e12144e1.svg"
+  },
+  {
+    "revision": "1e144968f237b9bfb6377cf1c010b818",
+    "url": "/kojo-cloth-page/static/media/kojo-primary-logo.1e144968.svg"
+  },
+  {
+    "revision": "35c55f95229186b0bea8a43bcbe97789",
+    "url": "/kojo-cloth-page/static/media/rompi-komunitas.35c55f95.svg"
+  },
+  {
+    "revision": "8b39765866ae99bc326f942a63acdcb0",
+    "url": "/kojo-cloth-page/static/media/syal.8b397658.svg"
+  },
+  {
+    "revision": "1fbd346c58c35f51fa1e12fb2eb66013",
+    "url": "/kojo-cloth-page/static/media/topi.1fbd346c.svg"
+  },
+  {
+    "revision": "65821326cc6cee0e422d11f098cb7a7f",
+    "url": "/kojo-cloth-page/static/media/unpad_example.65821326.svg"
+  }
+]);
